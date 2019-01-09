7c97a096d9c9a06fcec76158cac3cc468a1cfdfe
cqiu <cqiu2008@hotmail.com>
2019-01-10 07:16:42 +0800

    print get failed 1

--- vf/gui/ddd/tornadoWeb/aiwww/response.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-
-class Response():
-
-    def __init__(self, errorcode, message):
-        self.errorcode = errorcode
-        self.message = message
